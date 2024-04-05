8cb15d0248ae32431bb91cae15053f4ea1e43ec5
reubenJcorriea <reuben.corriea@gmail.com>
2024-04-05 16:57:35 -0400

    Searchbar is now connected to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,11 @@ import RecipeList from './components/RecipeList/RecipeList';
 import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => {
-  const handleSearch = (searchTerm: string) => {
-    console.log('Perform search with:', searchTerm);
-    // Implement your search logic here
-    // This could involve setting state that RecipeList uses to filter recipes
-  };
 
   return (
     <div className="popup">
       <Header />
-      <SearchBar onSearch={handleSearch} />
+      <SearchBar />
       <RecipeList />
       <Footer />
     </div>
--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './SearchBar.scss';
 
+// Utility hook for debouncing input values
 const useDebounce = (value: string, delay: number = 500): string => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
@@ -12,20 +13,30 @@ const useDebounce = (value: string, delay: number = 500): string => {
   return debouncedValue;
 };
 
-interface SearchBarProps {
-  onSearch: (searchTerm: string) => void; // Prop to handle search action
-}
-
-const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+const SearchBar: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const debouncedSearchTerm = useDebounce(searchTerm);
 
   useEffect(() => {
-    onSearch(debouncedSearchTerm); // Perform the search action with the debounced term
-  }, [debouncedSearchTerm, onSearch]);
+    if (debouncedSearchTerm) {
+      fetchRecipes(debouncedSearchTerm);
+    }
+  }, [debouncedSearchTerm]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
+  };
+
+  // Function to fetch recipes from the backend
+  const fetchRecipes = async (searchTerm: string) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/recipes/search?term=${encodeURIComponent(searchTerm)}`);
+      if (!response.ok) throw new Error('Problem fetching recipes');
+      const data = await response.json();
+      console.log(data); 
+    } catch (error) {
+      console.error('Failed to fetch recipes:', error);
+    }
   };
 
   return (
@@ -37,7 +48,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
         value={searchTerm}
         onChange={handleSearchChange}
       />
-      <button className="search-bar__button" type="button">Search</button>
+      <button className="search-bar__button" type="button" onClick={() => fetchRecipes(searchTerm)}>Search</button>
     </div>
   );
 };
